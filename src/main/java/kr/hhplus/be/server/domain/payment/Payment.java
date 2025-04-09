761ebe27453d738ba87c9a5a5a749f0fa327067a
jongwan <jongbell4@gmail.com>
2025-04-10 01:49:54 +0900

    [#35] 결제 도메인 기능 구현

--- src/main/java/kr/hhplus/be/server/domain/payment/Payment.java
+++ src/main/java/kr/hhplus/be/server/domain/payment/Payment.java
@@ -1,4 +1,77 @@
 package kr.hhplus.be.server.domain.payment;
 
+import kr.hhplus.be.server.domain.order.Order;
+import kr.hhplus.be.server.domain.order.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Payment {
+
+    private Long id;
+    private Order order;
+    private Integer paymentPrice;
+    private PaymentStatus paymentStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Payment(){}
+
+    public Payment(Order order, Integer paymentPrice, PaymentStatus paymentStatus) {
+        this.order = order;
+        this.paymentPrice = paymentPrice;
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Payment(Order order){
+        this.order = order;
+        this.paymentPrice = order.getTotalPrice();
+        this.paymentStatus = PaymentStatus.PAYMENT_PENDING;
+        this.order.updateOrderStatus(OrderStatus.PAYMENT_WAITING);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Integer getPaymentPrice() {
+        return paymentPrice;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void updatePaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public void pay(){
+        this.order.getOrderUser().deductPoint(this.paymentPrice);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id) && Objects.equals(order, payment.order) && Objects.equals(paymentPrice, payment.paymentPrice) && paymentStatus == payment.paymentStatus && Objects.equals(createdAt, payment.createdAt) && Objects.equals(updatedAt, payment.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order, paymentPrice, paymentStatus, createdAt, updatedAt);
+    }
 }
--- src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
+++ src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
@@ -1,7 +1,54 @@
 package kr.hhplus.be.server.domain.payment;
 
+import jakarta.persistence.NoResultException;
+import kr.hhplus.be.server.domain.order.Order;
+import kr.hhplus.be.server.domain.order.OrderRepository;
+import kr.hhplus.be.server.domain.user.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+
+    public PaymentService(
+            PaymentRepository paymentRepository,
+            UserRepository userRepository,
+            OrderRepository orderRepository) {
+        this.paymentRepository = paymentRepository;
+        this.userRepository = userRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional
+    public PaymentCreateCommand.Response save(PaymentCreateCommand command) {
+        Order order = orderRepository.findById(command.getOrderId())
+                .orElseThrow(NoResultException::new);
+
+        Payment payment = paymentRepository.save(new Payment(order));
+
+        order.registerPayment(payment);
+
+        return new PaymentCreateCommand.Response(payment);
+    }
+
+
+    @Transactional
+    public void processPayment(Long paymentId) {
+        Payment payment = paymentRepository.findById(paymentId);
+
+        payment.pay();
+
+
+
+
+
+    }
+
+    public Payment findById(Long paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
 }
