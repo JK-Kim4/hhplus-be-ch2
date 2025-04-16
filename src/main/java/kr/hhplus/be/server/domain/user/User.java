e8387a1c85a708d678a4228439735c09a95ea2ee
jongwan <jongbell4@gmail.com>
2025-04-17 02:28:56 +0900

    feat: 사용자 infra 구현

--- src/main/java/kr/hhplus/be/server/domain/user/User.java
+++ src/main/java/kr/hhplus/be/server/domain/user/User.java
@@ -1,6 +1,10 @@
 package kr.hhplus.be.server.domain.user;
 
+import jakarta.persistence.*;
+import kr.hhplus.be.server.domain.coupon.Coupon;
 import kr.hhplus.be.server.domain.point.Point;
+import kr.hhplus.be.server.domain.user.userCoupon.UserCoupon;
+import kr.hhplus.be.server.domain.user.userCoupon.UserCoupons;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,28 +12,49 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity(name = "user")
 @Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER)
     protected Point point;
+
+    @Embedded
+    private UserCoupons userCoupons;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public User(String name) {
+
         this.name = name;
+        userCoupons = new UserCoupons();
     }
 
     public User(Long id, String name){
         this.id = id;
         this.name = name;
+        userCoupons = new UserCoupons();
+    }
+
+    public boolean isAlreadyIssuedCoupon(Coupon coupon) {
+        return userCoupons.isAlreadyIssuedCoupon(coupon);
     }
 
     public Integer point(){
         return point.getAmount();
     }
 
+    public void addUserCoupon(UserCoupon userCoupon) {
+        this.userCoupons.addUserCoupon(userCoupon);
+    }
 
     public void chargePoint(Integer amount) {
         this.point.charge(amount);
