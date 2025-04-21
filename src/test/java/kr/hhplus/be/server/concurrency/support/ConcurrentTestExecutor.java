ed650f26fe62b6defa7bec6ac4e40675f2906c4c
jongwan <jongbell4@gmail.com>
2025-04-22 08:57:29 +0900

    feat: 동시성 테스트 공통 모듈 (ConcurrentTestExecutor)

---
+++ src/test/java/kr/hhplus/be/server/concurrency/support/ConcurrentTestExecutor.java
@@ -0,0 +1,28 @@
+package kr.hhplus.be.server.concurrency.support;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConcurrentTestExecutor {
+
+    public static void execute(int numberOfThreads, int counter, List<Runnable> tasks) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(counter);
+
+
+        for(int i = 0; i < counter; i ++){
+            for (Runnable task : tasks) {
+                executorService.execute(() ->{
+                    task.run();
+                    latch.countDown();
+                });
+            }
+        }
+
+        latch.await();
+        executorService.shutdown();
+    }
+
+}
