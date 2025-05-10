c78b1e3f20bb2680dec72e71c630f48f31cc13c7
jongwan <jongbell4@gmail.com>
2025-05-10 14:32:33 +0900

    feat: 분산락 테스트 코드

--- src/test/java/kr/hhplus/be/server/interfaces/RedissonLockExecutorTest.java
+++ src/test/java/kr/hhplus/be/server/interfaces/RedissonLockExecutorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
