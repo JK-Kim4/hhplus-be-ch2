f8db667bd674e7fdfef324555ba1957994878354
jongwan <jongbell4@gmail.com>
2025-05-15 15:14:56 +0900

    feat: 실시간 상품 판매 집계 통합 테스트 코드

---
+++ src/test/java/kr/hhplus/be/server/integration/salesStat/SalesStatProcessorTest.java
@@ -0,0 +1,78 @@
+package kr.hhplus.be.server.integration.salesStat;
+
+import kr.hhplus.be.server.application.salesStat.SalesStatProcessor;
+import kr.hhplus.be.server.domain.balance.Balance;
+import kr.hhplus.be.server.domain.balance.BalanceRepository;
+import kr.hhplus.be.server.domain.order.Order;
+import kr.hhplus.be.server.domain.order.OrderRepository;
+import kr.hhplus.be.server.domain.redis.RedisCommonStore;
+import kr.hhplus.be.server.domain.redis.RedisZSetStore;
+import kr.hhplus.be.server.domain.salesStat.TypedScore;
+import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.domain.user.UserRepository;
+import kr.hhplus.be.server.support.domainSupport.OrderDomainSupporter;
+import kr.hhplus.be.server.support.domainSupport.UserDomainSupporter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootTest
+@Testcontainers
+public class SalesStatProcessorTest {
+
+    @Autowired
+    SalesStatProcessor salesStatProcessor;
+
+    @Autowired
+    RedisZSetStore<String> redisZSetStore;
+
+    @Autowired
+    RedisCommonStore redisCommonStore;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BalanceRepository balanceRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    Order order;
+
+    @BeforeEach
+    void setup(){
+        User user = UserDomainSupporter.기본_사용자_생성();
+        userRepository.save(user);
+        Balance balance = UserDomainSupporter.사용자_잔고_생성(user.getId(), BigDecimal.valueOf(100_000));
+        balanceRepository.save(balance);
+        order = OrderDomainSupporter.사용자_고유번호를_전달받아_기본_주문_생성(user.getId());
+        orderRepository.save(order);
+
+
+        userRepository.flush();
+        balanceRepository.flush();
+        orderRepository.flush();
+    }
+
+    @Test
+    void 결제_완료된_주문의_고유번호를_전달받아_판매_상품_정보를_실시간_판매_집계_KEY에_추가한다(){
+        //given
+        String key = SalesStatProcessor.getDailySalesReportKey(LocalDate.now());
+        salesStatProcessor.dailySalesReportProcess(order.getId(), key);
+
+        //then
+        List<TypedScore<String>> zSetRangeWithScoresByKey = redisZSetStore.rangeWithScores(key);
+
+        Assertions.assertThat(zSetRangeWithScoresByKey.stream().map(TypedScore::value).toList())
+                .contains("1", "2", "3");
+    }
+
+}
