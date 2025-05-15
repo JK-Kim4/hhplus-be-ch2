ae0cdd03f54d27dc2fd9271ca114dc931e7b355e
jongwan <jongbell4@gmail.com>
2025-05-15 21:22:44 +0900

    feat: 판매 집계 스케줄러 / 파사드 수정

---
+++ src/main/java/kr/hhplus/be/server/interfaces/scheduler/salesStat/SalesStatScheduler.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.interfaces.scheduler.salesStat;
+
+import kr.hhplus.be.server.application.salesStat.SalesStatFacade;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class SalesStatScheduler {
+
+    private final SalesStatFacade salesStatFacade;
+
+    public SalesStatScheduler(
+            SalesStatFacade salesStatFacade) {
+        this.salesStatFacade = salesStatFacade;
+    }
+
+    //매일 자정 당일 판매 집계 영속화, 금일 판매 집계 redis key 생성
+    @Scheduled(cron = "0 0 0 * * *")
+    public void persistDailySalesStatsAtMidnight(){
+        salesStatFacade.persistRedisSalesStatToDb();
+    }
+}
