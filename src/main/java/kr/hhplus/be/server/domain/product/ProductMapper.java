4468b6cb3322eb07d58928805b94d48d8a5803f2
jongwan <jongbell4@gmail.com>
2025-05-15 15:17:07 +0900

    feat: 기타 수정

---
+++ src/main/java/kr/hhplus/be/server/domain/product/ProductMapper.java
@@ -0,0 +1,6 @@
+package kr.hhplus.be.server.domain.product;
+
+public class ProductMapper {
+
+
+}
---
+++ src/main/java/kr/hhplus/be/server/domain/salesStat/TypedScore.java
@@ -0,0 +1,6 @@
+package kr.hhplus.be.server.domain.salesStat;
+
+public record TypedScore<T>(T value, double score) {
+
+
+}
