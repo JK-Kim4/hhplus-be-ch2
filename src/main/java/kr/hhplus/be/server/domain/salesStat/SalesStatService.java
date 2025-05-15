47ad4ee7ffa766e14b853f4d1aa99a689cb50aa9
jongwan <jongbell4@gmail.com>
2025-05-15 15:14:36 +0900

    feat: 실시간 상품 판매 집계 기능 구현

--- src/main/java/kr/hhplus/be/server/domain/salesStat/SalesStatService.java
+++ src/main/java/kr/hhplus/be/server/domain/salesStat/SalesStatService.java
@@ -1,26 +1,17 @@
 package kr.hhplus.be.server.domain.salesStat;
 
-import kr.hhplus.be.server.domain.order.OrderRepository;
-import kr.hhplus.be.server.domain.payment.PaymentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//TODO 2025.05.07 SalesStatService To SalesStatFacade
 @Service
 public class SalesStatService {
 
     private final SalesStatRepository salesStatRepository;
-    private final PaymentRepository paymentRepository;
-    private final OrderRepository orderRepository;
 
     public SalesStatService(
-            SalesStatRepository salesStatRepository,
-            PaymentRepository paymentRepository,
-            OrderRepository orderRepository) {
+            SalesStatRepository salesStatRepository) {
         this.salesStatRepository = salesStatRepository;
-        this.paymentRepository = paymentRepository;
-        this.orderRepository = orderRepository;
     }
 
     public void createAll(SalesStatCommand.Creates command){
---
+++ src/main/java/kr/hhplus/be/server/interfaces/scheduler/SalesStatScheduler.java
@@ -0,0 +1,45 @@
+package kr.hhplus.be.server.interfaces.scheduler;
+
+import kr.hhplus.be.server.application.salesStat.SalesStatProcessor;
+import kr.hhplus.be.server.domain.salesStat.*;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@Transactional
+public class SalesStatScheduler {
+
+
+    private final RedisSalesStatService redisSalesStatService;
+    private final SalesStatService salesStatService;
+
+    public SalesStatScheduler(
+            RedisSalesStatService redisSalesStatService,
+            SalesStatService salesStatService) {
+        this.redisSalesStatService = redisSalesStatService;
+        this.salesStatService = salesStatService;
+    }
+
+    //매일 자정 당일 판매 집계 영속화, 금일 판매 집계 redis key 생성
+    @Scheduled(cron = "0 0 0 * * *")
+    public void persistDailySalesStatsAtMidnight(){
+        LocalDate targetDate = LocalDate.now().minusDays(1);
+        String targetKey = SalesStatProcessor.getDailySalesReportKey(targetDate);
+
+        SalesStatInfo.RedisTypedScoreSet typedScoreSet = redisSalesStatService.findReverseRangeWithScoresSetByKey(targetKey);
+
+        List<SalesStatInfo.SalesReport> list = typedScoreSet.getTypedScores().stream()
+                .map(report -> SalesStatInfo.SalesReport.of(
+                        Long.parseLong(report.value()),
+                        targetDate,
+                        (long) report.score()
+                )).toList();
+
+        //저장
+        salesStatService.createAll(SalesStatCommand.Creates.fromList(list));
+    }
+}
