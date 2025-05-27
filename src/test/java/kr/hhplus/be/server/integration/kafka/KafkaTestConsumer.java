7c8467cc54b6d6200aa24c49b6d8007fd3942f4e
jongwan <jongbell4@gmail.com>
2025-05-28 01:46:53 +0900

    feat: kafka message 테스트 코드

---
+++ src/test/java/kr/hhplus/be/server/integration/kafka/KafkaTestConsumer.java
@@ -0,0 +1,31 @@
+package kr.hhplus.be.server.integration.kafka;
+
+import kr.hhplus.be.server.common.event.PaymentCompletedEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class KafkaTestConsumer {
+
+    private final List<PaymentCompletedEvent> receivedMessages = new ArrayList<>();
+
+    @KafkaListener(
+            topics = KafkaTestPublisher.PAYMENT_COMPLETED_TOPIC_TEST,
+            groupId = "test-group"
+    )
+    public void listen(PaymentCompletedEvent message) {
+        receivedMessages.add(message);
+    }
+
+    public List<PaymentCompletedEvent> getReceivedMessages() {
+        return receivedMessages;
+    }
+
+    public void clearMessages() {
+        receivedMessages.clear();
+    }
+
+}
